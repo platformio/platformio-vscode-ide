2eb81a27b9bb27413a59429b817d222610de5a1b
Ivan Kravets <me@ikravets.com>
2018-04-09 16:55:59 -0700

    Wait a while when enterprise settings will be loaded

--- src/installer/manager.js
+++ src/installer/manager.js
@@ -26,21 +26,35 @@ export default class InstallationManager {
     this.stateStorage = new StateStorage(globalState, this.STORAGE_STATE_KEY);
 
     const config = vscode.workspace.getConfiguration('platformio-ide');
+    const defaultParams = {
+      pioCoreMinVersion: PIO_CORE_MIN_VERSION,
+      useBuiltinPIOCore: config.get('useBuiltinPIOCore'),
+      setUseBuiltinPIOCore: (value) => config.update('platformio-ide.useBuiltinPIOCore', value),
+      useDevelopmentPIOCore: config.get('useDevelopmentPIOCore'),
+      pythonPrompt: new PythonPrompt()
+    };
     this.stages = [
-      new pioNodeHelpers.installer.PlatformIOCoreStage(this.stateStorage, this.onDidStatusChange.bind(this), {
-        pioCoreMinVersion: PIO_CORE_MIN_VERSION,
-        useBuiltinPIOCore: config.get('useBuiltinPIOCore'),
-        setUseBuiltinPIOCore: (value) => config.update('platformio-ide.useBuiltinPIOCore', value),
-        useDevelopmentPIOCore: config.get('useDevelopmentPIOCore'),
-        pythonPrompt: new PythonPrompt(),
-        autorunPIOCmds: [
-          {
-            args: ['home', '--host', '__do_not_start__'],
-            when: 'post-install',
-            suppressError: true
+      new pioNodeHelpers.installer.PlatformIOCoreStage(
+        this.stateStorage,
+        this.onDidStatusChange.bind(this),
+        new Proxy(defaultParams, {
+          get: (obj, prop) => {
+            if (prop in obj) {
+              return obj[prop];
+            }
+            // wait a while when enterprise settings will be loaded
+            else if (prop === 'autorunPIOCmds') {
+              return [
+                {
+                  args: ['home', '--host', '__do_not_start__'],
+                  when: 'post-install',
+                  suppressError: true
+                }
+              ].concat(extension.getEnterpriseSetting('autorunPIOCoreCmds', []));
+            }
+            return undefined;
           }
-        ].concat(extension.getEnterpriseSetting('autorunPIOCoreCmds', []))
-      }),
+        })),
     ];
   }
 
@@ -83,7 +97,7 @@ export default class InstallationManager {
   }
 
   async install() {
-    await  Promise.all(this.stages.map(stage => stage.install()));
+    await Promise.all(this.stages.map(stage => stage.install()));
 
     const result = await vscode.window.showInformationMessage(
       'PlatformIO IDE has been successfully installed! Please reload window',
